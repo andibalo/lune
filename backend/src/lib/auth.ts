b7fa60cbdd7a6f2e42e6d99ae1b99d8cd4bccab2
Andi Usman Balo <andibalo213@gmail.com>
2025-05-23 07:30:32 +0700

    [BE] add admin plugin to better auth

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { nextCookies } from "better-auth/next-js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { createAuthMiddleware, APIError } from "better-auth/api";
-import { admin, customSession, magicLink } from "better-auth/plugins";
+import { admin } from "better-auth/plugins";
 
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+    plugins: [
+        admin()
+    ],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
